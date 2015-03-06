b7bd476ce475a0d172d65f6fe249aeaa55db97a8
Raphael Wunderlich <raphael.wunderlich@jura.ch>
2015-03-06 11:58:21 +0100

    Update version 1.0.9

--- crdppfportal/models.py
+++ crdppfportal/models.py
@@ -15,8 +15,3 @@ import sqlahelper
 from crdppf import db_config
 
 Base = sqlahelper.get_base()
-
-# Specific model definition
-class Tiles(Base):
-    __tablename__ = 'tiles'
-    __table_args__ = {'schema': db_config['schema'], 'autoload': True}
